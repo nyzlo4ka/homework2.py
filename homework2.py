15299143f2e593821628cf90932f70d164f317f0
Nyzlo4ka <nyzlo4ka@mail.ru>
2024-06-07 21:48:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homeworks = 12
+hours_spent = 1.5
+name_course = 'Python'
+average_time = (hours_spent / completed_homeworks)
+print("Курс:", name_course + ", Всего задач:", format(completed_homeworks) + ", Затрачено часов:", format(hours_spent) + ", Среднее время выполнения:", average_time, "часа.")
